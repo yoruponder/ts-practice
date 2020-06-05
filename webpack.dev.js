0d3cecfd952c6b5bd0d380ac0f956001de1e322f
ljm <ljm@addcn.com>
2020-06-05 13:58:31 +0800

    test

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,28 +1,29 @@
 const config = require('./webpack.config.js');
+const path = require('path');
 
 module.exports = {
-  ...config,
-  mode: "development",
-  module: {
-    rules: [
-      ...config.module.rules,
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader',
+    ...config,
+    mode: "development",
+    module: {
+        rules: [
+            ...config.module.rules,
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ]
+            },
         ]
-      },
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    host: 'localhost',
-    port: 5000,
-    quiet: false,
-    inline: true,
-    progress: true,
-    compress: true
-  }
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        host: 'localhost',
+        port: 5000,
+        quiet: false,
+        inline: true,
+        progress: true,
+        compress: true
+    }
 }
